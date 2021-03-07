ef6320c80b387d04e356cf5d2781eb7a21b9e4ed
Sandipan Chakraborty <sandipanchakraborty007@gmail.com>
2021-03-07 15:56:53 +0530

    adding header component and linking shop to shop page and logo to homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 
 import { Route, Switch } from "react-router-dom";
 
+import Header from "./components/header/header.component.jsx";
+
 import HomePage from "./pages/homepage/homepage.component.jsx";
 
 import ShopPage from "./pages/shop/shop.component.jsx";
@@ -10,6 +12,7 @@ import ShopPage from "./pages/shop/shop.component.jsx";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
