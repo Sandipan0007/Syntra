a1f01a937bab1f21fdde919d6920b42592e7a152
Sandipan Chakraborty <sandipanchakraborty007@gmail.com>
2021-04-04 02:28:38 +0530

    added cartIcon and cartDropdown to the application also implementented cartReducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,11 @@ import "./header.styles.scss";
 
 import { auth } from "../../firebase/firebase.utils";
 
-const Header = ({ currentUser }) => (
+import CartIcon from "../cart-icon/cart-icon.component";
+
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -31,12 +35,15 @@ const Header = ({ currentUser }) => (
           Sign In
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,10 @@
-import { combineReducers } from "redux";
+import { combineReducers } from "redux"; // for combining reducers have to import it.
 
 import userReducer from "./user/user.reducer";
 
+import cartReducer from "./cart/cart.reducer";
+
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
